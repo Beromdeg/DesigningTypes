b9edcaca7888fb4cb9467ad485887e2314904b2d
Beromdeg <30961531+Beromdeg@users.noreply.github.com>
2018-01-08 22:19:01 +0100

    Importing demo project

---
+++ DesigningTypes/Customer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//demo of Law of Demeter
+namespace DesigningTypes
+{
+    public class Customer{
+
+        public Customer(string firstName, string lastName){
+            FirstName = firstName;
+            lastName = lastName;
+            Wallet = new Wallet(1000m);
+        }
+
+        public string FirstName { get; }
+        public string LastName { get; }
+        // Demo voilation of LoD principle
+        //public Wallet Wallet { get; }
+
+        //not voilating LoD principle
+        private readonly Wallet Wallet;
+        public decimal GetPayment(decimal amount)
+        {
+            if (Wallet.MoneyAmount >= amount)
+            {
+                Wallet.WithdrawAmount(amount);
+                return amount;
+            }
+            return 0; //exit
+        }
+    }
+
+    public class PizzaBoy
+    {
+        public void DeliverPizza(decimal costOfPizza, Customer customer)
+        {
+        // *** Voilation of LoD principle giving pizzaboy class accesss to customer wallet ***//
+            //Wallet w = customer.Wallet;
+            //if(w.MoneyAmount > costOfPizza)
+            //{
+            //    w.WithdrawMoney(costOfPizza);
+            //}
+            //else
+            //{
+            //    //cant buy
+            //}
+
+        //*** Following LoD, we isolate PizzaBoy from Wallet****// 
+            decimal payment = customer.GetPayment(costOfPizza);
+            if(payment == costOfPizza)
+            {
+                //Thank you and exit
+            }
+            else
+            {
+                //Cant buy
+            }
+        }
+    }
+}
---
+++ DesigningTypes/Wallet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesigningTypes
+{
+    public class Wallet
+    {
+        public decimal MoneyAmount { get; private set; }
+
+        public Wallet(decimal moneyAmount)
+        {
+            MoneyAmount = moneyAmount;
+        }
+        
+        public void AddMoney(decimal amount)
+        {
+            MoneyAmount += amount;
+        }
+
+        public void WithdrawAmount(decimal amount)
+        {
+            MoneyAmount -= amount;
+        }
+    }
+}
