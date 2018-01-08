9cfa24c985202ddacdd61640e62c7e8030796811
Beromdeg <30961531+Beromdeg@users.noreply.github.com>
2018-01-08 22:38:05 +0100

    Comment added on wallet

--- DesigningTypes/Wallet.cs
+++ DesigningTypes/Wallet.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace DesigningTypes
 {
+    //Walllet class could have been private
     public class Wallet
     {
         public decimal MoneyAmount { get; private set; }
